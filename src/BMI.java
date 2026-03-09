a1d9b55dbf08498acd25174188d5ff7f40ca8ad3
harivijay <harivijay2884@gmail.com>
2026-03-09 13:34:23 +0530

    BMI MODULE COMPLETED

---
+++ src/BMI.java
@@ -0,0 +1,18 @@
+public class BMI {
+
+    public void  BMI(double height,double weight){
+
+        double bmi = weight / (height * height);
+
+        System.out.println("Your BMI is: " + bmi);
+        if(bmi < 18.5)
+            System.out.println("Category: Underweight");
+        else if(bmi < 25)
+            System.out.println("Category: Normal Weight");
+        else if(bmi < 30)
+            System.out.println("Category: Overweight");
+        else
+            System.out.println("Category: Obese");
+
+    }
+}
