a10fc8746330530364d1cbd765f0d62182600695
harivijay <harivijay2884@gmail.com>
2026-03-18 00:21:02 +0530

    connection established

---
+++ src/DBConnection.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    public static Connection getConnection() {
+
+        try {
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/gymfreak",
+                    "root",
+                    "123root123"
+            );
+
+            return con;
+
+        } catch (Exception e) {
+            System.out.println("DB Error: " + e);
+            return null;
+        }
+    }
+}
